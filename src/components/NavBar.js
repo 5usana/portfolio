ce4d2af82e229fe17060fc0493f689b8e2aabf57
Susana Martins <scmartins87@gmail.com>
2021-05-05 09:12:45 -0700

    added social media links to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -44,6 +44,12 @@ function NavBar() {
 					HOME 
 					</HeaderOne>
 			</Link>
+				
+				<a href="https://twitter.com/Susana_debugs" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/twitter.svg" alt="Susana_debugs" height="30" width="40" /></a>
+				<a href="https://www.linkedin.com/in/susanacmartins/" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg" alt="susanacmartins" height="30" width="40" /></a>
+ 				<a href="https://www.instagram.com/susanacodes/?hl=en" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg" alt="susanawashere" height="30" width="40" /></a>
+
+				
 			<HomeNav>
 				<Link to='/about'>
 					<AboutButton>
