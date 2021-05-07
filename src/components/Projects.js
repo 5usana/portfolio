74f2bdd0cfd094ce142d983642b0065b990514eb
Susana Martins <scmartins87@gmail.com>
2021-05-07 14:03:19 -0700

    added project 4 and link to deployed app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ function Projects(props) {
         <div>
             <h2> Project 4 </h2>
             <p>Word Vomit is a one liner blog to document events most worthy of remembrance. It's satisfying being able to look back on your accomplishments, even defeats, and memories in-between but the thought of writing out a whole page can be daunting. Here you are encouraged to write out your thoughts, of the day in only a line or so without the pressure of filling out an entire page.</p>
+            <a href='https://wvblog.herokuapp.com/' target='blank'>WV Blog </a>
 
             {/* <h2> Project 3 </h2>
             <p>Word Vomit is a one liner blog to document events most worthy of remembrance. It's satisfying being able to look back on your accomplishments, even defeats, and memories in-between but the thought of writing out a whole page can be daunting. Here you are encouraged to write out your thoughts, of the day in only a line or so without the pressure of filling out an entire page.</p> */}
