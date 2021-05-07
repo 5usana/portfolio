1f97b99075a7edeb3047a7fcc2a8b7a38a0a738f
Susana Martins <scmartins87@gmail.com>
2021-05-07 14:08:01 -0700

    added project 2 and deployed link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,8 +7,9 @@ function Projects(props) {
             <p>Word Vomit is a one liner blog to document events most worthy of remembrance. It's satisfying being able to look back on your accomplishments, even defeats, and memories in-between but the thought of writing out a whole page can be daunting. Here you are encouraged to write out your thoughts, of the day in only a line or so without the pressure of filling out an entire page.</p>
             <a href='https://wvblog.herokuapp.com/' target='blank'>WV Blog </a>
 
-            {/* <h2> Project 3 </h2>
-            <p>Word Vomit is a one liner blog to document events most worthy of remembrance. It's satisfying being able to look back on your accomplishments, even defeats, and memories in-between but the thought of writing out a whole page can be daunting. Here you are encouraged to write out your thoughts, of the day in only a line or so without the pressure of filling out an entire page.</p> */}
+            <h2> Project 2 </h2>
+            <p>Unsolicited Advice from friends or strangers can get tiresome so why not let my app decide for you? Don't like the advice you get? That's ok, give it another go and find the adice you came for!</p>
+            <a href='https://unsolicited-advice.herokuapp.com/' target='blank'>Unsolicited Advice </a>
 
             
         </div>
