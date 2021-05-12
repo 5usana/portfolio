ba501913267cab40f3f4c8936f1ae73e79277842
Susana Martins <scmartins87@gmail.com>
2021-05-12 10:01:43 -0700

    added globalstyle for background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
+import img from './images/wall.jpg';
+
+
+const GlobalStyle = createGlobalStyle`
+body {
+	margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-image: url(${img});
+  background-color: teal; 
+}
+`;
 
 function App() {
   return (
     <>
       <Router>
+      <GlobalStyle />
         <Switch>
           <Route exact path='/' render={Home} />
           <Route path='/about' component={AboutMe} />
